55071c3bb417a79e9add4916d00b5a0b0e998fa8
David Wells <daverwells@gmail.com>
2012-05-22 11:46:11 -0400

    update test and build

--- getTestValues.m
+++ getTestValues.m
@@ -5,15 +5,20 @@ someTriangle = [0.1190    0.9597    0.5853; 0.4984    0.3404    0.2238];
 
 [C B b Th] = changeOfBasis(someTriangle);
 % note that C2 is the transpose of C1 by the new convention.
-[C2 B2 b2 Th2] = ArgyrisMaps(someTriangle);
+[C2 B2 b2 Th2] = ArgyrisMapsMex(someTriangle);
+
+argyris = evalArgy(C, qV);
+argyris2 = ArgyrisFunctionsMex(C2, qV.values);
 
 [dx dy] = evalGradArgy(C, B, qV);
-[dx2 dy2] = ArgyrisGradients(C2, B2, qV.derivativeX, qV.derivativeY);
+[dx2 dy2] = ArgyrisGradientsMex(C2, B2, qV.derivativeX, qV.derivativeY);
 
 [dxx dxy dyy] = evalHessArgy(C, Th, qV);
-[dxx2 dxy2 dyy2] = ArgyrisHessians(C2, Th2, qV.derivativeXX, qV.derivativeXY, ...
+[dxx2 dxy2 dyy2] = ArgyrisHessiansMex(C2, Th2, qV.derivativeXX, qV.derivativeXY, ...
                                    qV.derivativeYY);
 
+disp('max difference in function values:')
+max(max(abs(argyris - argyris2)))
 disp('max difference in gradients:')
 max([max(max(abs(dx2 - dx))), max(max(abs(dy2 - dy)))])
 disp('max difference in second derivatives:')
