a7ff481d5dd229c61436793a2186c6a8d0db79d9
drwells <drwells@vt.edu>
2012-06-11 10:02:17 -0400

    fixed last build instruction

--- make.m
+++ make.m
@@ -1,4 +1,4 @@
 mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-O3 -ansi -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64' ArgyrisMapsMex.c -lmwblas
 mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-O3 -ansi -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64' ArgyrisFunctionsMex.c -lmwblas
 mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-O3 -ansi -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64' ArgyrisGradientsMex.c -lmwblas
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-O3 -ansi -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64'ArgyrisHessiansMex.c -lmwblas
+mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-O3 -ansi -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64' ArgyrisHessiansMex.c -lmwblas
