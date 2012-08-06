0f5019d42190a8e55bffe4b1cf80a1cdc51d1522
drwells <drwells@vt.edu>
2012-08-05 21:22:15 -0400

    REFACTOR moved all includes to a single header file

--- order_logic.h
+++ order_logic.h
@@ -24,16 +24,14 @@ double __d_one = 1;
         #define DGEMM_WRAPPER(m, n, k, A, B, C) \
         dgemm(&(__c_N), &(__c_N), &(n), &(m), &(k), &(__d_one), B, &(n), A, \
              &(k), &(__d_zero), C, &(n))
-        /* wrap C.T := B * A.T (or C = A * B.T).
+        /* wrap C.T := B*A.T (or C = A*B.T).
+         *
          * m : number of rows of A; n : number of columns of B.T; k : number of
-         * columns of A / rows of B.T
+         * columns of A / rows of B.T. For sanity, define D, E, and F to be the
+         * transposes of A, B, and C.
+         *
+         * Therefore E.T is n x k, D is k x m, and F is n x m
          */
-/* m : number of rows in A; n : number of columns in B.T; k : columns of A/rows
- * of B.T
- * m : number of columns in D; n : number of rows in E.T; k : number of rows in
- * D, columns of E.T
- */
-/* therefore E.T is n x k, D is k x m, and F is n x m*/
         #define DGEMM_WRAPPER_NT(m, n, k, D, E, F) \
         dgemm(&(__c_T), &(__c_N), &(n), &(m), &(k), &(__d_one), E, &(k), D, \
               &(k), &(__d_zero), F, &(n))
@@ -47,10 +45,11 @@ double __d_one = 1;
         #define DGEMM_WRAPPER(m, n, k, A, B, C) \
         dgemm(&(__c_N), &(__c_N), &(m), &(n), &(k), &(__d_one), A, &(m), B, \
               &(k), &(__d_zero), C, &(m))
-        /* wrap C.T := B * A.T */
+        /* wrap C := A*B.T */
         #define DGEMM_WRAPPER_NT(m, n, k, A, B, C) \
         dgemm(&(__c_N), &(__c_T), &(m), &(n), &(k), &(__d_one), A, &(m), B, \
               &(n), &(__d_zero), C, &(m))
+        /* wrap C.T := A*B.T + C */
         #define DGEMM_WRAPPER_NT_ADD_C(m, n, k, A, B, C) \
         dgemm(&(__c_N), &(__c_T), &(m), &(n), &(k), &(__d_one), A, &(m), B, \
               &(n), &(__d_one), C, &(m))
