3a3559ecd56e3d0a188d2f63c56c9427483723ab
drwells <drwells@vt.edu>
2012-08-06 12:35:52 -0400

    BUG added correct declaration of array index to please Matlab

--- matrix_mass.c
+++ matrix_mass.c
@@ -7,7 +7,7 @@ void multiply_by_diagonal(const int rows, const int cols,
                           double* restrict diagonal, double* restrict matrix);
 
 void ap_matrix_mass(double* restrict C, double* restrict B,
-                    double* restrict ref_values, double* restrict weights,
+                    double* restrict ref_functions, double* restrict weights,
                     LAPACKINDEX quad_points, double* restrict mass)
 {
 
@@ -16,10 +16,13 @@ void ap_matrix_mass(double* restrict C, double* restrict B,
         double functions_scaled[21*quad_points];
         double weights_scaled[quad_points];
 
+        /* stuff for DGEMM. */
+        LAPACKINDEX i_twentyone = 21;
+
         const double jacobian = fabs(B[ORDER(0, 0, 2, 2)]*B[ORDER(1, 1, 2, 2)] -
                                      B[ORDER(0, 1, 2, 2)]*B[ORDER(1, 0, 2, 2)]);
 
-        ap_global_functions(C, ref_values, quad_points, values);
+        ap_global_functions(C, ref_functions, quad_points, functions);
 
         /* scale the weights by the jacobian. */
         for (i = 0; i < quad_points; i++) {
@@ -34,7 +37,7 @@ void ap_matrix_mass(double* restrict C, double* restrict B,
          */
         multiply_by_diagonal(21, quad_points, weights_scaled, functions_scaled);
         DGEMM_WRAPPER_NT(i_twentyone, i_twentyone, quad_points,
-                         functions_scaled, functions, stiffness);
+                         functions_scaled, functions, mass);
 }
 
 void multiply_by_diagonal(const int rows, const int cols,
