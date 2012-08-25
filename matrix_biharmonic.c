866d4db9cd8084a3f9a2e76b38a259f11f18ab86
drwells <drwells@vt.edu>
2012-08-24 22:15:57 -0400

    FEATURE full implementation of C library

--- matrix_biharmonic.c
+++ matrix_biharmonic.c
@@ -1,12 +1,3 @@
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
-
-#include "global_hessians.c"
-
-void multiply_by_diagonal(const int rows, const int cols,
-                          double* restrict diagonal, double* restrict matrix);
-
 void ap_matrix_biharmonic(double* restrict C, double* restrict B,
                           double* restrict Th,
                           double* restrict ref_dxx, double* restrict ref_dxy,
@@ -46,31 +37,4 @@ void ap_matrix_biharmonic(double* restrict C, double* restrict B,
         multiply_by_diagonal(21, quad_points, weights_scaled, dxx);
         DGEMM_WRAPPER_NT(i_twentyone, i_twentyone, quad_points, dxx, dyy,
                          biharmonic);
-}
-
-void multiply_by_diagonal(const int rows, const int cols,
-                          double* restrict diagonal, double* restrict matrix) {
-        /*
-         * Multiply a matrix by a diagonal matrix (represented as a flat array
-         * of n values). The diagonal structure is exploited (so that we use
-         * n^2 multiplications instead of n^3); the product looks something
-         * like
-         *
-         *     sage: A = matrix([[a,b,c], [d,e,f],[g,h,i]]);
-         *     sage: w = matrix([[z1,0,0],[0,z2,0],[0,0,z3]]);
-         *     sage: A * w
-         *      => [a*z1 b*z2 c*z3]
-         *         [d*z1 e*z2 f*z3]
-         *         [g*z1 h*z2 i*z3]
-         */
-        int i, j;
-        /* traverse the matrix in the correct order. */
-#ifdef USE_COL_MAJOR
-        for (j = 0; j < cols; j++)
-                for (i = 0; i < rows; i++)
-#else
-        for (i = 0; i < rows; i++)
-                for (j = 0; j < cols; j++)
-#endif
-                        matrix[ORDER(i, j, rows, cols)] *= diagonal[j];
 }
