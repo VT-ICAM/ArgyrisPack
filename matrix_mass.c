ecc1505bb6fca6723401acaf4cc42e6650ad0846
drwells <drwells@vt.edu>
2012-08-24 22:15:57 -0400

    FEATURE full implementation of C library

--- matrix_mass.c
+++ matrix_mass.c
@@ -1,11 +1,3 @@
-#include <string.h>
-#include <stdlib.h>
-
-#include "global_functions.c"
-
-void multiply_by_diagonal(const int rows, const int cols,
-                          double* restrict diagonal, double* restrict matrix);
-
 void ap_matrix_mass(double* restrict C, double* restrict B,
                     double* restrict ref_functions, double* restrict weights,
                     LAPACKINDEX quad_points, double* restrict mass)
@@ -38,30 +30,4 @@ void ap_matrix_mass(double* restrict C, double* restrict B,
         multiply_by_diagonal(21, quad_points, weights_scaled, functions_scaled);
         DGEMM_WRAPPER_NT(i_twentyone, i_twentyone, quad_points,
                          functions_scaled, functions, mass);
-}
-
-void multiply_by_diagonal(const int rows, const int cols,
-                          double* restrict diagonal, double* restrict matrix) {
-        /*
-         * Multiply a matrix by a diagonal matrix (represented as a flat array
-         * of n values). The diagonal structure is exploited; the product looks
-         * something like
-         *
-         *     sage: A = matrix([[a,b,c], [d,e,f],[g,h,i]]);
-         *     sage: w = matrix([[z1,0,0],[0,z2,0],[0,0,z3]]);
-         *     sage: A * w
-         *      => [a*z1 b*z2 c*z3]
-         *         [d*z1 e*z2 f*z3]
-         *         [g*z1 h*z2 i*z3]
-         */
-        int i, j;
-        /* traverse the matrix in the correct order. */
-#ifdef USE_COL_MAJOR
-        for (j = 0; j < cols; j++)
-                for (i = 0; i < rows; i++)
-#else
-        for (i = 0; i < rows; i++)
-                for (j = 0; j < cols; j++)
-#endif
-                        matrix[ORDER(i, j, rows, cols)] *= diagonal[j];
 }
--- matrix_stiffness.c
+++ matrix_stiffness.c
@@ -1,11 +1,3 @@
-#include <string.h>
-#include <stdlib.h>
-
-#include "global_gradients.c"
-
-void multiply_by_diagonal(const int rows, const int cols,
-                          double* restrict diagonal, double* restrict matrix);
-
 void ap_matrix_stiffness(double* restrict C, double* restrict B,
                          double* restrict ref_dx, double* restrict ref_dy,
                          double* restrict weights,
@@ -46,30 +38,4 @@ void ap_matrix_stiffness(double* restrict C, double* restrict B,
                          stiffness);
         DGEMM_WRAPPER_NT_ADD_C(i_twentyone, i_twentyone, quad_points, dy_scaled,
                                dy, stiffness);
-}
-
-void multiply_by_diagonal(const int rows, const int cols,
-                          double* restrict diagonal, double* restrict matrix) {
-        /*
-         * Multiply a matrix by a diagonal matrix (represented as a flat array
-         * of n values). The diagonal structure is exploited; the product looks
-         * something like
-         *
-         *     sage: A = matrix([[a,b,c], [d,e,f],[g,h,i]]);
-         *     sage: w = matrix([[z1,0,0],[0,z2,0],[0,0,z3]]);
-         *     sage: A * w
-         *      => [a*z1 b*z2 c*z3]
-         *         [d*z1 e*z2 f*z3]
-         *         [g*z1 h*z2 i*z3]
-         */
-        int i, j;
-        /* traverse the matrix in the correct order. */
-#ifdef USE_COL_MAJOR
-        for (j = 0; j < cols; j++)
-                for (i = 0; i < rows; i++)
-#else
-        for (i = 0; i < rows; i++)
-                for (j = 0; j < cols; j++)
-#endif
-                        matrix[ORDER(i, j, rows, cols)] *= diagonal[j];
 }
