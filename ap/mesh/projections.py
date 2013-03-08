f43e6feb5a51a9632192ac0b4b29f0f82a427c88
drwells <drwells@vt.edu>
2013-03-08 14:34:07 -0500

    FEATURE added latitudinal offsets

--- ap/mesh/projections.py
+++ ap/mesh/projections.py
@@ -2,7 +2,8 @@
 import numpy as np
 from math import pi
 
-def lambert_azimuthal(coordinate_tripples, longitude_offset = pi/8):
+def lambert_azimuthal(coordinate_tripples, longitude_offset = pi/8,
+                      latitude_offset = pi/8):
     """
     An area-preserving projection from geophysical coordinates to the
     plane.
@@ -27,13 +28,15 @@ def lambert_azimuthal(coordinate_tripples, longitude_offset = pi/8):
     z = coordinate_tripples[2]
     # convert to latitude / longitude coordinates.
     rho = np.sqrt(x**2) + np.sqrt(y**2) + np.sqrt(z**2)
-    azimuth = np.arctan2(y,x)                    # same as longitude
+    azimuth = np.arctan2(y,x)                  # same as longitude
     inclination = -1*(np.arccos(z/rho) - pi/2) # same as latitude
 
     # project to 2D.
-    k = np.sqrt(2/(1 + np.cos(inclination)*np.cos(azimuth - longitude_offset)))
+    k = np.sqrt(2/(1 + np.cos(inclination - latitude_offset)
+                   *np.cos(azimuth - longitude_offset)))
 
-    x_projected = (k*np.cos(inclination)*np.sin(azimuth - longitude_offset))
-    y_projected = (k*np.sin(inclination))
+    x_projected = (k*np.cos(inclination - latitude_offset)
+                   *np.sin(azimuth - longitude_offset))
+    y_projected = k*np.sin(inclination)
 
     return np.array([x_projected, y_projected])
