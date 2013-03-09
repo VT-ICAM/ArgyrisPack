4f6cc68f4b0857b98308965ad9e96c6484fae5ba
drwells <drwells@vt.edu>
2013-03-08 19:09:26 -0500

    REFACTOR use the function syntax to clean up the call.

--- make.m
+++ make.m
@@ -1,14 +1,16 @@
 % Build file for MEX files.
-cd ./ap/mex/
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-fPIC -O3 -DUSE_COL_MAJOR -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64 -I../numerical/' apLocalFunctionsMex.c -lmwblas
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-fPIC -O3 -DUSE_COL_MAJOR -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64 -I../numerical/' apLocalGradientsMex.c -lmwblas
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-fPIC -O3 -DUSE_COL_MAJOR -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64 -I../numerical/' apLocalHessiansMex.c -lmwblas
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-fPIC -O3 -DUSE_COL_MAJOR -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64 -I../numerical/' apGlobalFunctionsMex.c -lmwblas
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-fPIC -O3 -DUSE_COL_MAJOR -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64 -I../numerical/' apGlobalGradientsMex.c -lmwblas
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-fPIC -O3 -DUSE_COL_MAJOR -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64 -I../numerical/' apGlobalHessiansMex.c -lmwblas
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-fPIC -O3 -DUSE_COL_MAJOR -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64 -I../numerical/' apGlobalMapsMex.c -lmwblas
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-fPIC -O3 -DUSE_COL_MAJOR -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64 -I../numerical/' apMatrixMassMex.c -lmwblas
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-fPIC -O3 -DUSE_COL_MAJOR -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64 -I../numerical/' apMatrixStiffnessMex.c -lmwblas
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-fPIC -O3 -DUSE_COL_MAJOR -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64 -I../numerical/' apMatrixBetaplaneMex.c -lmwblas
-mex -largeArrayDims -DLAPACKINDEX=mwSignedIndex CFLAGS='-fPIC -O3 -DUSE_COL_MAJOR -std=c99 -D_GNU_SOURCE -pthread -m64 -fexceptions -D_FILE_OFFSET_BITS=64 -I../numerical/' apMatrixBiharmonicMex.c -lmwblas
-cd ../../
+mexFiles = {'apLocalFunctionsMex.c', 'apLocalGradientsMex.c', ...
+            'apLocalHessiansMex.c', 'apGlobalFunctionsMex.c', ...
+            'apGlobalGradientsMex.c', 'apGlobalHessiansMex.c', ...
+            'apGlobalMapsMex.c', 'apMatrixMassMex.c', ...
+            'apMatrixStiffnessMex.c', 'apMatrixBetaplaneMex.c', ...
+            'apMatrixBiharmonicMex.c'};
+
+for i=1:length(mexFiles)
+    file = ['./ap/mex/' mexFiles{i}];
+    mex(['-I', matlabroot '/extern/include/'], ['-I' './ap/mex/'], ...
+        ['-I' './ap/numerical/'], '-DLAPACKINDEX=mwSignedIndex', ...
+        '-DUSE_COL_MAJOR', '-DMWINDEX=mwIndex', ['CFLAGS=\' ...
+        '$CFLAGS -fPIC -O3 -std=c99'], 'LDFLAGS=\$LDFLAGS', '-lmwblas', ...
+        '-largeArrayDims', file);
+end
