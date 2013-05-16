618dc6062734230db17d08933ddf0d81ae57e841
drwells <drwells@vt.edu>
2013-05-16 00:39:14 -0400

    Update with new naming convention.

--- ap/argyris_pack.jl
+++ ap/argyris_pack.jl
@@ -1,50 +1,47 @@
-# ------------------------------------------------------------------------------
-# Load the shared object file.
-# ------------------------------------------------------------------------------
 libap = dlopen("libargyris_pack.so")
 
-__ap_local_functions   = dlsym(libap, :ap_local_functions)
-__ap_local_gradients   = dlsym(libap, :ap_local_gradients)
-__ap_local_hessians    = dlsym(libap, :ap_local_hessians)
-__ap_global_maps       = dlsym(libap, :ap_global_maps)
-__ap_global_functions  = dlsym(libap, :ap_global_functions)
-__ap_global_gradients  = dlsym(libap, :ap_global_gradients)
-__ap_global_hessians   = dlsym(libap, :ap_global_hessians)
-__ap_matrix_mass       = dlsym(libap, :ap_matrix_mass)
-__ap_matrix_stiffness  = dlsym(libap, :ap_matrix_stiffness)
-__ap_matrix_biharmonic = dlsym(libap, :ap_matrix_biharmonic)
+__ap_ref_values         = dlsym(libap, :ap_ref_values)
+__ap_ref_gradients      = dlsym(libap, :ap_ref_gradients)
+__ap_ref_hessians       = dlsym(libap, :ap_ref_hessians)
+__ap_physical_maps      = dlsym(libap, :ap_physical_maps)
+__ap_physical_values    = dlsym(libap, :ap_physical_values)
+__ap_physical_gradients = dlsym(libap, :ap_physical_gradients)
+__ap_physical_hessians  = dlsym(libap, :ap_physical_hessians)
+__ap_matrix_mass        = dlsym(libap, :ap_matrix_mass)
+__ap_matrix_stiffness   = dlsym(libap, :ap_matrix_stiffness)
+__ap_matrix_biharmonic  = dlsym(libap, :ap_matrix_biharmonic)
 
 # ------------------------------------------------------------------------------
 # Julia interfaces to the .so file.
 # ------------------------------------------------------------------------------
-function ap_local_functions{T}(x::Vector{T}, y::Vector{T})
+function ap_ref_values{T}(x::Vector{T}, y::Vector{T})
 # evaluate the 21 Argyris basis functions at quadrature points specified by
 # 'points'.
     check_size(x,y)
 
     function_values = zeros(21,size(x)[1])
-    ccall(__ap_local_functions, Void,
+    ccall(__ap_ref_values, Void,
           (Ptr{Float64}, Ptr{Float64}, Int32, Ptr{Float64}),
           x, y, length(x), function_values)
 
     return function_values
 end
 
-function ap_local_gradients{T}(x::Vector{T}, y::Vector{T})
+function ap_ref_gradients{T}(x::Vector{T}, y::Vector{T})
 # evaluate the derivatives of the 21 Argyris basis functions at quadrature
 # quadrature points specified by 'points'.
     check_size(x,y)
 
     dx = zeros(21,size(x)[1])
     dy = zeros(21,size(x)[1])
-    ccall(__ap_local_gradients, Void,
+    ccall(__ap_ref_gradients, Void,
           (Ptr{Float64}, Ptr{Float64}, Int32, Ptr{Float64}, Ptr{Float64}),
           x, y, size(x)[1], dx, dy)
 
     return dx, dy
 end
 
-function ap_local_hessians{T}(x::Vector{T}, y::Vector{T})
+function ap_ref_hessians{T}(x::Vector{T}, y::Vector{T})
 # evaluate the second derivatives of the 21 Argyris basis functions at quadrature
 # quadrature points specified by 'points'.
     check_size(x,y)
@@ -52,7 +49,7 @@ function ap_local_hessians{T}(x::Vector{T}, y::Vector{T})
     dxx = zeros(21,size(x)[1])
     dxy = zeros(21,size(x)[1])
     dyy = zeros(21,size(x)[1])
-    ccall(__ap_local_hessians, Void,
+    ccall(__ap_ref_hessians, Void,
           (Ptr{Float64}, Ptr{Float64}, Int32, Ptr{Float64}, Ptr{Float64},
            Ptr{Float64}),
           x, y, length(x), dxx, dxy, dyy)
@@ -60,8 +57,8 @@ function ap_local_hessians{T}(x::Vector{T}, y::Vector{T})
     return dxx, dxy, dyy
 end
 
-function ap_global_maps{T}(x::Vector{T}, y::Vector{T})
-# Use the change-of-basis matrix C to convert reference values to global values.
+function ap_physical_maps{T}(x::Vector{T}, y::Vector{T})
+# Use the change-of-basis matrix C to convert reference values to physical values.
     check_size(x,y)
     if length(x) != 3
         error("There should be three corners to a triangle.")
@@ -71,7 +68,7 @@ function ap_global_maps{T}(x::Vector{T}, y::Vector{T})
     B = zeros(2,2)
     b = zeros(2)
     Th = zeros(3,3)
-    ccall(__ap_global_maps, Void,
+    ccall(__ap_physical_maps, Void,
           (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64},
            Ptr{Float64}),
           x, y, C, B, b, Th)
@@ -79,20 +76,20 @@ function ap_global_maps{T}(x::Vector{T}, y::Vector{T})
     return C, B, b, Th
 end
 
-function ap_global_functions(C, ref_values)
-# Use the change-of-basis matrix C to convert reference values to global values.
+function ap_physical_values(C, ref_values)
+# Use the change-of-basis matrix C to convert reference values to physical values.
     check_transformations(C)
     check_reference_values(ref_values)
 
     function_values = zeros(21,size(ref_values)[2])
-    ccall(__ap_global_functions, Void, (Ptr{Float64}, Ptr{Float64}, Int32,
+    ccall(__ap_physical_values, Void, (Ptr{Float64}, Ptr{Float64}, Int32,
           Ptr{Float64}), C, ref_values, size(ref_values)[2],
           function_values)
 
     return function_values
 end
 
-function ap_global_gradients(C, B, ref_dx, ref_dy)
+function ap_physical_gradients(C, B, ref_dx, ref_dy)
 # evaluate the derivatives of the 21 Argyris basis functions given values of
 # the derivatives on the reference triangle.
     check_transformations(C,B)
@@ -100,7 +97,7 @@ function ap_global_gradients(C, B, ref_dx, ref_dy)
 
     dx = zeros(size(ref_dx))
     dy = zeros(size(ref_dx))
-    ccall(__ap_global_gradients, Void,
+    ccall(__ap_physical_gradients, Void,
           (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Int32,
            Ptr{Float64}, Ptr{Float64}),
           C, B, ref_dx, ref_dy, size(ref_dx)[2], dx, dy)
@@ -108,7 +105,7 @@ function ap_global_gradients(C, B, ref_dx, ref_dy)
     return dx, dy
 end
 
-function ap_global_hessians(C, Th, ref_dxx, ref_dxy, ref_dyy)
+function ap_physical_hessians(C, Th, ref_dxx, ref_dxy, ref_dyy)
 # Evaluate second derivatives of the basis functions given reference values.
     check_transformations(C)
     if size(Th) != (3,3)
@@ -119,7 +116,7 @@ function ap_global_hessians(C, Th, ref_dxx, ref_dxy, ref_dyy)
     dxx = zeros(size(ref_dxx))
     dxy = zeros(size(ref_dxx))
     dyy = zeros(size(ref_dxx))
-    ccall(__ap_global_hessians, Void,
+    ccall(__ap_physical_hessians, Void,
           (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64},
            Int32, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}),
           C, Th, ref_dxx, ref_dxy, ref_dyy, size(ref_dxx)[2], dxx, dxy, dyy)
@@ -128,22 +125,22 @@ function ap_global_hessians(C, Th, ref_dxx, ref_dxy, ref_dyy)
 end
 
 function ap_matrix_mass(C, B, ref_functions, weights)
-# Evaluate the local mass matrix given reference values.
+# Evaluate the ref mass matrix given reference values.
     check_transformations(C,B)
-    check_reference_values(ref_functions)
-    check_weights(ref_functions, weights)
+    check_reference_values(ref_values)
+    check_weights(ref_values, weights)
 
     mass = zeros(21,21)
     ccall(__ap_matrix_mass, Void,
           (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Int32,
            Ptr{Float64}),
-          C, B, ref_functions, weights, size(weights)[1], mass)
+          C, B, ref_values, weights, size(weights)[1], mass)
 
     return mass
 end
 
 function ap_matrix_stiffness(C, B, ref_dx, ref_dy, weights)
-# Evaluate the local stiffness matrix given reference values.
+# Evaluate the ref stiffness matrix given reference values.
     check_transformations(C,B)
     check_reference_values(ref_dx, ref_dy)
     check_weights(ref_dx, weights)
@@ -158,7 +155,7 @@ function ap_matrix_stiffness(C, B, ref_dx, ref_dy, weights)
 end
 
 function ap_matrix_biharmonic(C, B, Th, ref_dxx, ref_dxy, ref_dyy, weights)
-# Evaluate the local biharmonic matrix given reference values.
+# Evaluate the ref biharmonic matrix given reference values.
     check_transformations(C,B,Th)
     check_reference_values(ref_dxx, ref_dxy, ref_dyy)
     check_weights(ref_dxx, weights)
