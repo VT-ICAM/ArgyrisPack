083e22fb5910d6064e952da02b97298a195f5a15
Erich L Foster <erichlf@gmail.com>
2013-06-18 10:38:24 +0200

    Added Miller Cylindrical projection. Also, fixed a bug in Lambert Azimuthal where the radius was calculated incorrectly. I added a cartesian to lat lon function.

--- ap/mesh/projections.py
+++ ap/mesh/projections.py
@@ -2,15 +2,15 @@
 import numpy as np
 from math import pi
 
-def lambert_azimuthal(coordinate_tripples, longitude_offset = pi/8,
-                      latitude_offset = pi/8):
+def lambert_lonal(coordinate_triples, longitude_offset = pi/8.,
+                      latitude_offset = pi/8.):
     """
     An area-preserving projection from geophysical coordinates to the
     plane.
 
     Required Arguments
     -------------------
-    * coordinate_tripples : 3-column, N row tripples of sphere
+    * coordinate_triples : 3-column, N row triples of sphere
       (cartesian) coordinates.
 
     Optional Arguments
@@ -21,22 +21,64 @@ def lambert_azimuthal(coordinate_tripples, longitude_offset = pi/8,
     Output
     ------
     * the Lambert Azimuthal projection (not scaled) of the
-    coordinate_tripples.
+    coordinate_triples.
     """
-    x = coordinate_tripples[0]
-    y = coordinate_tripples[1]
-    z = coordinate_tripples[2]
-    # convert to latitude / longitude coordinates.
-    rho = np.sqrt(x**2) + np.sqrt(y**2) + np.sqrt(z**2)
-    azimuth = np.arctan2(y,x)                  # same as longitude
-    inclination = -1*(np.arccos(z/rho) - pi/2) # same as latitude
+    lat, lon = convert_to_latlon(coordinate_triples)
 
     # project to 2D.
-    k = np.sqrt(2/(1 + np.cos(inclination - latitude_offset)
-                   *np.cos(azimuth - longitude_offset)))
+    k = np.sqrt(2./(1. + np.cos(lat - latitude_offset)
+                   *np.cos(lon - longitude_offset)))
 
-    x_projected = (k*np.cos(inclination - latitude_offset)
-                   *np.sin(azimuth - longitude_offset))
-    y_projected = k*np.sin(inclination)
+    x_projected = (k*np.cos(lat - latitude_offset)
+                   *np.sin(lon - longitude_offset))
+    y_projected = k*np.sin(lat)
 
     return np.array([x_projected, y_projected])
+
+def miller_cylindrical(coordinate_triples):
+    """
+    A modified mercador projection from geophysical coordinates to the
+    plane.
+
+    Required Arguments
+    -------------------
+    * coordinate_triples : 3-column, N row triples of sphere
+      (cartesian) coordinates.
+
+    Output
+    ------
+    * the Miller cylindrical projection (not scaled) of the
+    coordinate_triples.
+    """
+
+    lat, lon = convert_to_latlon(coordinate_triples) 
+    lon_0 = 0.                           # center meridian
+    #now use miller cylindrical projection
+    x_projected = lon - lon_0
+    y_projected = 5./4.*np.log(np.tan(pi/4.+2./5.*lat))
+
+    return np.array([x_projected, y_projected])
+
+def convert_to_latlon(coordinate_triples): 
+    """
+    converts cartesian coordinates (x,y,z) to lat,lon
+
+    Required Arguments
+    -------------------
+    * coordinate_triples : 3-column, N row triples of sphere
+      (cartesian) coordinates.
+
+    Output
+    ------
+    * the corresponding latitude and longitude for a given point x,y,z
+    """
+    x = coordinate_triples[0]
+    y = coordinate_triples[1]
+    z = coordinate_triples[2]
+
+    # convert to latitude / longitude coordinates.
+    R = np.sqrt(x**2 + y**2 + z**2)           # Radius of the Earth
+    lon = np.arctan2(y,x)                     # longitude
+    lat = np.arcsin(z/R)                      # latitude
+
+    return np.array([lat, lon])
